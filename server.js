2ef325b2ea4fea4b5fc67aa722b48f76a7245a66
MrLaxman09 <laxmankumar6089@gmail.com>
2026-02-12 15:58:44 +0530

    first commit

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 // ======== CORS ======== //
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:8080"],
+    origin: ["http://localhost:5173", "http://localhost:8080", "http://localhost:8081"],
     credentials: true,
   }),
 );
